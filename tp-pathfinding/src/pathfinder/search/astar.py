f7aba1831834742ad59c08e131ca819beb7bd774
flaibani <34102650+flaibani@users.noreply.github.com>
2024-04-22 14:26:15 -0300

    Algoritmo ASTAR

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -16,12 +16,72 @@ class AStarSearch:
             Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        #node = Node("", grid.start, 0)
+        node = Node("", state=grid.start, cost=0, parent=None, action=None)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
-        
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
-        return NoSolution(explored)
+        # Initialize the reached dictionary to be empty
+        reached = {} 
+
+        # Add the node to the reached dictionary
+        reached[node.state] = 0
+
+        # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, reached)
+
+        # Initialize the frontier with the initial node
+        # In this example, the frontier is a queue
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, node.cost + grid.h_manhatan(node.state))
+
+        while True:
+
+            #  Fail if the frontier is empty
+            if frontier.is_empty():
+                return NoSolution(reached)
+
+            # Remove a node from the frontier
+            node = frontier.pop()
+
+            # Return if the node contains a goal state
+            # In this example, the goal test is run
+            # before adding a new node to the frontier
+            if node.state == grid.end:
+                return Solution(node, reached)
+
+            successors = grid.get_neighbours(node.state)
+            for (dir, new_state) in successors.items():
+
+                cost = node.cost + grid.get_cost(new_state)
+                # Check if the successor is not reached
+                if new_state not in reached or cost < reached[new_state]:
+                        
+                    # Initialize the son node
+                    new_node = Node("", new_state,
+                                cost,
+                                parent=node, action=dir)
+
+                    # Mark the successor as reached
+                    reached[new_state] = cost
+
+                    # Add the new node to the frontier
+                    frontier.add(new_node, node.cost + grid.h_manhatan(node.state))
+
+'''
+function GRAPH-ASTAR(problema,h) return solución o fallo
+    n₀ ← NODO(problema.estado-inicial, None, None, 0)
+    frontera ← ColaPrioridad()
+    frontera.encolar(n₀, n₀.costo+h(n₀))
+    alcanzados ← {n₀.estado: n₀.costo}
+    do
+        if frontera.vacía() then return fallo
+        n ← frontera.desencolar()
+        if problema.test-objetivo(n.estado) then return solución(n)
+        forall a in problema.acciones(n.estado) do
+            s’ ← problema.resultado(n.estado, a)
+            c’ ← n.costo + problema.costo-individual(n.estado,a)
+            if s’ is not in alcanzados or c’ < alcanzados[s’] then
+                n’ ← Nodo(s’, n, a, c’)
+                alcanzados[s’] ← c’
+                frontera.encolar(n’, n’.costo+h(n’))
+'''
