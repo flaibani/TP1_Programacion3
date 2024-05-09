fb3fc402f9fdfdd39cd0350e1c859f737116ac39
flaibani <34102650+flaibani@users.noreply.github.com>
2024-05-09 19:57:58 -0300

    Tabú sin terminar

--- tp-tsp/search.py
+++ tp-tsp/search.py
@@ -164,3 +164,100 @@ class Tabu(LocalSearch):
     """Algoritmo de busqueda tabu."""
 
     # COMPLETAR
+    def solve(self, problem: OptProblem):
+        """Resuelve un problema de optimizacion con ascension de colinas.
+        Argumentos:
+        ==========
+        problem: OptProblem
+            un problema de optimizacion
+        """
+        # Inicio del reloj
+        start = time()
+
+        # Arrancamos del estado inicial
+        actual = problem.init
+        value = problem.obj_val(problem.init)
+        better = actual
+        tabu_list = []  # Lista Tabú
+        tabu_size = 100  # Tamaño de la lista Tabú
+
+        while self.niters < 500:
+            # Determinar las acciones que se pueden aplicar
+            # y las diferencias en valor objetivo que resultan
+            diff = problem.val_diff(actual)
+
+            # Buscar las acciones que generan el mayor incremento de valor obj
+            max_acts = [act for act, val in diff.items() if val ==
+                        max(diff.values())]
+
+            # Elegir una accion aleatoria
+            act = choice(max_acts)
+
+
+
+            actual = problem.result(actual, act)
+            value = value + diff[act]
+            self.niters += 1
+
+        # Asignar la solución encontrada
+        self.tour = actual
+        self.value = value
+        # Calcular el tiempo de ejecución
+        end = time()
+        self.time = end - start
+
+
+'''
+    def solve(self, problem: OptProblem):
+        """Resuelve un problema de optimización con búsqueda Tabú."""
+        # Inicio del reloj
+        start = time()
+        print(start)
+        # Arrancar desde un estado inicial
+        actual = problem.init
+        value = problem.obj_val(actual)
+        while True:
+            # Determinar las acciones que se pueden aplicar
+            # y las diferencias en valor objetivo que resultan
+            diff = problem.val_diff(actual)
+            # Excluir las acciones en la lista Tabú
+            diff_tabu = {act: val for act, val in diff.items() if act not in self.tabu_list}
+            # Si no hay acciones disponibles, terminar la búsqueda
+            if not diff_tabu:
+                break
+            # Buscar la acción que genera el mayor incremento de valor objetivo
+            act = max(diff_tabu, key=diff_tabu.get)
+            print("act",act)
+            #act = choice(max_acts)
+            # Actualizar la lista Tabú
+            self.tabu_list.append(act)
+            print(self.tabu_list)
+            if len(self.tabu_list) > self.tabu_size:
+                self.tabu_list.pop(0)
+            # Moverse al sucesor
+            actual = problem.result(actual, act)
+            value += diff[act]
+            self.niters += 1
+        # Asignar la solución encontrada
+        self.tour = actual
+        self.value = value
+        # Calcular el tiempo de ejecución
+        end = time()
+        self.time = end - start
+'''
+
+'''
+function BÚSQUEDA-TABÚ(problema) return estado
+    actual ← problema.estado-inicial
+    mejor ← actual
+    tabu ← inicialmente vacía
+    while no se cumpla el criterio de parada do
+        sucesores ← {problema.resultado(actual,a) for a є problema.acciones(actual)}
+        #algun cambio
+        no_tabues ← {sucesor for sucesor є sucesores if sucesor no es tabú}
+        sucesor ← argmax(problema.f, no_tabues}
+        if problema.h(mejor) < problema.h(sucesor) then mejor ← sucesor
+        actualizar la lista tabú
+        actual ← sucesor
+    return mejor
+'''
