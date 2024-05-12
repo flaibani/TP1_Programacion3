20a947156e9feee1ac3b6b0940de41d3a7a9df1c
flaibani <34102650+flaibani@users.noreply.github.com>
2024-05-12 18:32:34 -0300

    Tabú - últimas mejoras

--- tp-tsp/main.py
+++ tp-tsp/main.py
@@ -37,7 +37,7 @@ def main() -> None:
 
     # Resolver el TSP con cada algoritmo
     for algo in algos.values():
-        algo.solve(p,args.filename)
+        algo.solve(p)
 
     # Mostrar resultados por linea de comandos
     print("Valor:", "Tiempo:", "Iters:", "Algoritmo:", sep="\t\t")
@@ -54,52 +54,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-'''
-Ejemplo — TSP.
-Comenzar con un tour con la ciudad inicial y agregar las
-ciudades de a una, en el orden en que se visitan en el estado
-objetivo.
-
-function LOCAL-SEARCH(problema) return estado
-    s ← problema.estado-inicial
-    while no se cumple criterio de parada do:
-        S’ ← {problema.resultado(s,a) for
-            a є problema.acciones(s)}
-        elegir un buen sucesor s’ є S’
-        s ← s’
-    return s
-
-Proponer una formulación de estados completa y una función
-objetivo para el problema del viajante (TSP).    
-
-Notación.
-{c₁,…,cn}: conjunto de ciudades, suponer que c₁ es la inicial.
-dist(ci,cj): distancia en km entre todo par de ciudades ci y cj.
-
-En esta formulación, un estado es una (n+1)-tupla
-tal que:
-● (c₁ es la ciudad inicial).
-● (c₁ es la ciudad final).
-● para todo i ε {2,…,n}, es una ciudad diferente de {c₂,…,cn}.
-el cual representa el orden en que se recorren las ciudades: se inicia en
-aaaaaaa, luego se visita , y así sucesivamente, hasta visitar a y
-finalmente regresar a .
-El número total de estados es (n-1).(n-2).….2.1 = (n-1)!
-
-Una acción es un par (i,k) є {2,…,n}², con i<k, que
-representa intercambiar de lugar la i-ésima ciudad del tour
-por la j-ésima ciudad del tour.
-
-pag 49
-
-function ASCENSIÓN-COLINAS(problema) return estado
-    s ← problema.estado-inicial
-    do
-        S’ ← {problema.resultado(s,a) for
-            є problema.acciones(s)}
-        s’ ← argmax(problema.f, S’)
-        if (problema.f(s’) ≤ problema.f(s)) then return s
-    s ← s’
-
-'''
--- tp-tsp/search.py
+++ tp-tsp/search.py
@@ -15,12 +15,11 @@ No viene implementado, se debe completar.
 No viene implementado, se debe completar.
 """
 
-
 from __future__ import annotations
 from problem import OptProblem
 from random import choice
 from time import time
-
+import math
 
 class LocalSearch:
     """Clase que representa un algoritmo de busqueda local general."""
@@ -45,7 +44,7 @@ class HillClimbing(LocalSearch):
     El criterio de parada es alcanzar un optimo local.
     """
 
-    def solve(self, problem: OptProblem, instancia):
+    def solve(self, problem: OptProblem):
         """Resuelve un problema de optimizacion con ascension de colinas.
 
         Argumentos:
@@ -90,7 +89,7 @@ class HillClimbing(LocalSearch):
 
 class HillClimbingReset(LocalSearch):
 
-    def solve(self, problem: OptProblem, instancia):
+    def solve(self, problem: OptProblem):
         """Resuelve un problema de optimizacion con ascension de colinas.
 
         Argumentos:
@@ -107,18 +106,11 @@ class HillClimbingReset(LocalSearch):
         actual = problem.init
         value = problem.obj_val(problem.init)
         self.value = float('-inf')
-        #Se repite 20 veces porque así se garantiza 
-        #alcanzar el mayor máximo local que permite el  
-        #ascenso de colina y a su vez el consumo 
-        #de tiempo no es significativo. 
-        if (instancia=='instances/ar24.tsp'):
-            print("repeat 20")
-            repeat = 20
-        elif (instancia=='instances/att48.tsp'):
-            print("repeat 70")
-            repeat = 70    
-        else:
-            repeat =   45  
+
+        # Criterio: el número de nodos determinada el número de repeticiones 
+        repeat = int(problem.G.number_of_nodes()) * 2
+        print(repeat)
+
         while True:
 
             # Determinar las acciones que se pueden aplicar
@@ -162,7 +154,7 @@ class Tabu(LocalSearch):
     """Algoritmo de busqueda tabu."""
 
     # COMPLETAR
-    def solve(self, problem: OptProblem, instancia):
+    def solve(self, problem: OptProblem):
         """Resuelve un problema de optimizacion con ascension de colinas.
         Argumentos:
         ==========
@@ -178,34 +170,40 @@ class Tabu(LocalSearch):
         better = actual
         value_better = value
         tabu_list = []  # Lista Tabú
-        tabu_size = 3  # Tamaño de la lista Tabú
+        tabu_size = math.floor(problem.G.number_of_nodes()/8)  # Tamaño de la lista Tabú
+        print("size", tabu_size)
+        counter = 0
 
-        while self.niters < 100:
+        while True: #self.niters < 2000:
             # Determinar las acciones que se pueden aplicar
             # y las diferencias en valor objetivo que resultan
             diff = problem.val_diff(actual)
 
             # Buscar las acciones que generan el mayor incremento de valor obj
             # y que no están en la lista Tabu
-            max_acts = [act for act, val in diff.items() if val ==
-                        max(diff.values())]
-            #max_acts = [act for act, val in diff.items()]
-            print("max_acts", max_acts)
+            #max_acts = [act for act, val in diff.items() if val ==
+            #            max(diff.values())]
+
+            # Ordenar los valores de diff de mayor a menor y quedarse con los 10 mayores
+            max_acts = [act for act, val in diff.items() if val in sorted(diff.values(), reverse=True)[:10]]
+            #print("max_acts", max_acts)
 
             # lista de acciones que no están en la lista tabu
             no_tabu = [act for act in max_acts if act not in tabu_list]
             #print("no_tabu", no_tabu)
 
             # Si no hay acciones disponibles, terminar la búsqueda
-            if not no_tabu:
+            #if not no_tabu:
+            if len(no_tabu) == 0:
                 print("pasos", self.niters)
                 break
             # Elegir una accion aleatoria
             act = choice(no_tabu)
+            #print("accion seleccionada", act)
 
             # Actualizar la lista Tabú
             tabu_list.append(act)
-            print("tabu_list", tabu_list)
+            #print("tabu_list", tabu_list)
 
             # Si pasó el largo máximode la lista, elimina el primer elemento ingresado
             if len(tabu_list) > tabu_size:
@@ -213,13 +211,17 @@ class Tabu(LocalSearch):
 
             actual = problem.result(actual, act)
             value = value + diff[act]
-            print("value, value_better", value , value_better)
+            #print("value, value_better", value , value_better)
             self.niters += 1
             if value_better < value:
+                counter = 0
                 better = actual
                 value_better = value
-                print("actualiza better", value_better)
-
+                #print("actualiza better", value_better)
+            else:
+                counter += 1
+            if counter >= 1000:
+                break
         # Asignar la solución encontrada
         self.tour = better
         self.value = value_better
@@ -227,47 +229,30 @@ class Tabu(LocalSearch):
         end = time()
         self.time = end - start
 
+'''
+Ejemplo — TSP.
+Comenzar con un tour con la ciudad inicial y agregar las
+ciudades de a una, en el orden en que se visitan en el estado
+objetivo.
 
-'''
-    def solve(self, problem: OptProblem):
-        """Resuelve un problema de optimización con búsqueda Tabú."""
-        # Inicio del reloj
-        start = time()
-        print(start)
-        # Arrancar desde un estado inicial
-        actual = problem.init
-        value = problem.obj_val(actual)
-        while True:
-            # Determinar las acciones que se pueden aplicar
-            # y las diferencias en valor objetivo que resultan
-            diff = problem.val_diff(actual)
-            # Excluir las acciones en la lista Tabú
-            diff_tabu = {act: val for act, val in diff.items() if act not in self.tabu_list}
-            # Si no hay acciones disponibles, terminar la búsqueda
-            if not diff_tabu:
-                break
-            # Buscar la acción que genera el mayor incremento de valor objetivo
-            act = max(diff_tabu, key=diff_tabu.get)
-            print("act",act)
-            #act = choice(max_acts)
-            # Actualizar la lista Tabú
-            self.tabu_list.append(act)
-            print(self.tabu_list)
-            if len(self.tabu_list) > self.tabu_size:
-                self.tabu_list.pop(0)
-            # Moverse al sucesor
-            actual = problem.result(actual, act)
-            value += diff[act]
-            self.niters += 1
-        # Asignar la solución encontrada
-        self.tour = actual
-        self.value = value
-        # Calcular el tiempo de ejecución
-        end = time()
-        self.time = end - start
-'''
+function LOCAL-SEARCH(problema) return estado
+    s ← problema.estado-inicial
+    while no se cumple criterio de parada do:
+        S’ ← {problema.resultado(s,a) for
+            a є problema.acciones(s)}
+        elegir un buen sucesor s’ є S’
+        s ← s’
+    return s
 
-'''
+function ASCENSIÓN-COLINAS(problema) return estado
+    s ← problema.estado-inicial
+    do
+        S’ ← {problema.resultado(s,a) for
+            є problema.acciones(s)}
+        s’ ← argmax(problema.f, S’)
+        if (problema.f(s’) ≤ problema.f(s)) then return s
+    s ← s’
+
 function BÚSQUEDA-TABÚ(problema) return estado
     actual ← problema.estado-inicial
     mejor ← actual
